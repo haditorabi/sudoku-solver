1ad919bbc849eaa0d7ab37622b50555e5bdc2a71
haditorabi <haditorabi1984@gmail.com>
2021-07-30 15:39:45 -0400

    add getRows getColumns getBlocks

--- main.js
+++ main.js
@@ -21,13 +21,69 @@ for (let x = 0; x < 9; x++) {
     squares[x+"_"+y] = square;
   }
 }
+function getRows () {
+  let rowValues = [];
+  for (let y = 0; y < 9; y++) {
+    rowValues[y] = [];
+    for (let x = 0; x < 9; x++) {
+      let squareValue = squares[x+"_"+y].innerText;
+      rowValues[y].push(squareValue);
+    }
+  }
+  return rowValues;
+}
+
+function getColumns () {
+  let colValues = [];
+  for (let x = 0; x < 9; x++) {
+    colValues[x] = [];
+      for (let y = 0; y < 9; y++) {
+      let squareValue = squares[x+"_"+y].innerText;
+      colValues[x].push(squareValue);
+    }
+  }
+  return colValues;
+}
+function getBlocks () {
+  let blockValues = [];
+  let blockNumber = 0;
+  for (let brs = 0; brs < 3; y++) {
+    let blockColStart = Math.floor(brs / 3) * 3;
+    for (let x = 0; x < 9; x++) {
+  let blockRowStart = Math.floor(x / 3) * 3;
+  for (let y = 0; y < 9; y++) {
+    for (let x = 0; x < 9; x++) {
+      if((y%3) ==2  || (x%3) == 2) {
+        blockNumber = blockNumber+1;
+      }
+      blockValues[blockNumber] = [];
+      // let squareValue = squares[x+"_"+y].innerText;
+      // blockValues[x].push(squareValue);
+      blockValues[blockNumber].push(blockNumber);
+    }
+  }
+  return blockValues;
+}
+function checkIfDuplicateValues(w){
+  return new Set(w).size !== w.length 
+}
+
+function validationRows(rowNumber = null) {
+    // let hasDuplicate = checkIfDuplicateValues(rowValues);
+
+    console.log(getBlocks());
+}
+function validationColumns() {
+}
+function validationBlocks() {
+}
 const puzzle = {
-	"5_0": 2,
+  "5_0": 2,
 	"6_0": 9,
 	"8_0": 4,
 	"0_1": 9,
 	"4_1": 6,
-	"0_2": 6,
+	"0_2": 7,
 	"1_2": 2,
 	"5_2": 7,
 	"0_3": 7,
@@ -56,6 +112,9 @@ for (let key in puzzle) {
   square.solved = true
 }
 // console.log(squares);
+function solvePuzzle() {
+  validationRows();
+}
 var solveButton = document.getElementById("solve-button");
 var resetButton = document.getElementById("reset-button");
 var validateButton = document.getElementById("validate-button");
@@ -75,3 +134,4 @@ validateButton.onclick = function() {
   validatePuzzle();
   console.timeEnd("validate");
 }
+solvePuzzle();
